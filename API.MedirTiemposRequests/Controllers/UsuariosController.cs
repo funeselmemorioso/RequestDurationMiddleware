7727d453d4b8e8f5e0bdefec180d86bc60a546e6
NICOLAS B <niculinpiculin@hotmail.com>
2023-07-27 13:33:27 -0300

    init

---
+++ API.MedirTiemposRequests/Controllers/UsuariosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.MedirTiemposRequests.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        List<string> usuarios;
+
+        public UsuariosController()
+        {
+            this.usuarios = new List<string>(); 
+            usuarios.Add("un user0");
+            usuarios.Add("otro user1");
+            usuarios.Add("otro user2");
+            usuarios.Add("otro user3");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {     
+            Thread.Sleep(6000);
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            Thread.Sleep(2000);
+            return Ok(usuarios[id]);
+        }
+    }
+}
---
+++ API.MedirTiemposRequests/Middlewares/RequestsDurationMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace API.MedirTiemposRequests.Middlewares
+{
+    // BASADO EN :
+    // https://www.c-sharpcorner.com/article/measuring-and-reporting-the-response-time-of-an-asp-net-core-api/
+
+
+    public class RequestsDurationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestsDurationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task InvokeAsync(HttpContext context)
+        {
+            var reloj = new Stopwatch();
+            reloj.Start();
+
+            context.Response.OnStarting(() => {
+
+                reloj.Stop();
+                var tiempoEnResponder = reloj.ElapsedMilliseconds;               
+                context.Response.Headers["request-duracion"] = tiempoEnResponder.ToString() + " ms";
+                
+                // Mandar a una base desde aqui en un context (no inyectar por constructor)
+
+                return Task.CompletedTask;
+            });
+            
+            return this._next(context);           
+        }
+    }
+}
